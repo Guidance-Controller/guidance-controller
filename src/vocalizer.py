3f2fcc5622547d10c9d821a33c0adcff050bb268
Karl Velazquez <93351243+karlvelazquez@users.noreply.github.com>
2025-05-18 18:09:15 -0400

    guidance controller experiment without F2L so far

--- src/vocalizer.py
+++ src/vocalizer.py
@@ -8,25 +8,33 @@ class Vocalizer:
         self.buffer_period = buffer_period
         self.last_utter_time = time.time() - buffer_period
 
+    def is_uttering(self) -> bool:
+        result = subprocess.run(
+            ['pgrep', 'say'],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        return result.returncode == 0
+
     def utter(self, phrase: str, interupt: bool = False) -> bool:
         if phrase == "":
             return False
 
         if interupt:
             os.system(f'killall say; say "{phrase}" &')
-
             self.last_utter_time = time.time()
             return True
+
         elif time.time() - self.last_utter_time >= self.buffer_period:
-            pgrep_command = subprocess.run(
-                ['pgrep', 'say'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            if pgrep_command.returncode == 0:
+            if self.is_uttering():
                 return False
 
             subprocess.Popen(['say', f'"{phrase}"'],
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
 
             self.last_utter_time = time.time()
             return True
+
         else:
             return False
